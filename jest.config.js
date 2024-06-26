92ae89b3aace95e8ad4a130392a9ab71c8996401
Lucas Pollice <lp1935@nyu.edu>
2024-06-26 16:09:38 -0400

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { baseJestConfig } = require('@attentive/tools-config/jest');
-
 const config = {
-  ...baseJestConfig,
   rootDir: './',
   testMatch: ['<rootDir>/src/**/*.test.js'],
 };
